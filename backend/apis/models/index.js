9798ac053d5edc38f3eb67e01e0e6c3d38537fed
nientun <pjakapan@gmail.com>
2019-12-16 07:42:10 +0700

    update develop.json database

--- backend/apis/models/index.js
+++ backend/apis/models/index.js
@@ -1,38 +1,39 @@
-var config    = require('config');
-var fs        = require("fs");
-var path      = require("path");
+var config = require("config");
+var fs = require("fs");
+var path = require("path");
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize(
-    config.database.name,
-    config.database.user,
-    config.database.password, {
-        host: config.database.host,
-        dialect: config.database.driver,
-        freezeTableName: true,
-        pool: {
-            max: 9,
-            min: 0,
-            idle: 10000
-        }
+  config.database.name,
+  config.database.user,
+  config.database.password,
+  {
+    host: config.database.host,
+    port: config.database.port,
+    dialect: config.database.driver,
+    freezeTableName: true,
+    pool: {
+      max: 9,
+      min: 0,
+      idle: 10000
     }
+  }
 );
 
 var db = {};
 
-fs
-    .readdirSync(__dirname)
-    .filter(function(file) {
-        return (file.indexOf(".") !== 0) && (file !== "index.js");
-    })
-    .forEach(function(file) {
-        var model = sequelize["import"](path.join(__dirname, file));
-        db[model.name] = model;
-    });
+fs.readdirSync(__dirname)
+  .filter(function(file) {
+    return file.indexOf(".") !== 0 && file !== "index.js";
+  })
+  .forEach(function(file) {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(function(modelName) {
-    if ("associate" in db[modelName]) {
-        db[modelName].associate(db);
-    }
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
